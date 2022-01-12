21f188335cb4bdd0269ee621b10ca451362a89d3
birmantu <birmantu@gmail.com>
2022-01-12 11:53:12 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerHub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t mantu0328/java-maven-app:1.2 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push mantu0328/java-maven-app:1.2'
